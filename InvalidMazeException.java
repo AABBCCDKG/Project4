3969ca621e0d968fb149778853ab739d57d9f015
dongwang <156266097+AABBCCDKG@users.noreply.github.com>
2024-04-07 15:52:23 -0400

    Add files via upload

---
+++ InvalidMazeException.java
@@ -0,0 +1,15 @@
+/**
+ * A program Project4
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024 -- Project4-- </p>
+ *
+ * @author Dong Wang Purdue CS
+ * @version Mar 27, 2024
+ */
+public class InvalidMazeException extends Exception{
+
+    public InvalidMazeException(String message) {
+        super(message);
+    }
+
+}
